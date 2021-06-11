f0f68cb0cc0fda70eb66b96813ef7ffa6b1aef1b
Alessandro <alessandro.barros@tecnospeed.com.br>
2021-06-11 14:51:46 -0300

    Iniciando Testes

---
+++ tests/src/controller/controller.test.js
@@ -0,0 +1,13 @@
+const Cepcontroller = require('../../../src/controller/CepController')
+
+describe('CepController', () => {
+    it('should be able to register a new cep', () =>{
+
+        
+        expect().toBe();
+    });
+
+    // it('should be able to register a new cep', () =>{
+    //     expect().toBe();
+    // });
+})
